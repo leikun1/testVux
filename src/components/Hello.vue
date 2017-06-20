dc7dec17ae16920d096a6eeb55b8932b9adea215
GraceKeven <415397381@qq.com>
2017-06-20 15:48:30 +0800

    msg

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }} World!</h1>
-    <h2>Essential Links</h2>
+    <h2>Essential Links----{{ctrl}}</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
@@ -25,7 +25,8 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Hello'
+      msg: 'Hello',
+      ctrl: 'Git'
     }
   }
 }
